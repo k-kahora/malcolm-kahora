c6043a3cf02ecd7a571fe76ea85a0aef822d93df
malcolmkahora <mango22MJK@gmail.com>
2021-10-26 15:26:53 -0400

    First commit

---
+++ build-site.el
@@ -0,0 +1,42 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+(require 'ox-publish)
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list ; top level list is all project configs and each seconf level list is confogs for each project
+       (list "my-org-site"
+             :recursive t ; when looking at content folder looks at all sub directories this all regeerates all files from scratch
+             :base-directory "./content" ; all org files
+             :publishing-directory "./public" ; outpit directory
+             :publishing-function 'org-html-publish-to-html
+	            :with-author nil           ;; Don't include author name
+             :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-toc t                ;; Include a table of contents
+             :section-numbers nil       ;; Don't include section numbers
+             :time-stamp-file nil)))    ;; Don't include time stamp in file
+
+;; Generate the site output
+
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+
+
+(org-publish-all t)
+
+(message "the build is done")
